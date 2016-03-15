2d57f5d730ad3bd0631ab8bab4eb1fe19cec230d
misear <zhong.ch22@gmail.com>
2016-03-16 00:51:55 +0800

    创建代码

---
+++ AppWebApi/Controllers/TemplateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBHelper;
+
+namespace AppWebApi.Controllers
+{
+    public class TemplateController : ApiController
+    {
+        DBService db = new DBService();
+
+        [ActionName("TemplateCount")]
+        public int GetTemplateCount()
+        {
+            int count = db.GetTemplateCount();
+            return count;
+        }
+
+        [ActionName("TemplateList")]
+        public string GetTemplateList()
+        {
+            string json = db.GetTemplateList();
+            return json;
+        }
+    }
+}
---
+++ DBHelper/DBService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DBHelper
+{
+    public class DBService
+    {
+        private string strConString = ConfigurationManager.ConnectionStrings["AppConString"].ToString();
+        public int GetTemplateCount()
+        {
+
+            int count = 0;
+            MySqlConnection con = new MySqlConnection(strConString);
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("select count(1) from viewdata", con);
+                con.Open();
+                object obj = cmd.ExecuteScalar();
+                con.Close();
+                count = Convert.ToInt32(obj);
+                return count;
+            }
+            catch (Exception ex)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            return count;
+        }
+
+        public string GetTemplateList()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(strConString);
+                MySqlDataAdapter da = new MySqlDataAdapter("Select * From ViewData", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                string json = JsonConvert.SerializeObject(dt, new DataTableConverter());
+                return json;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
